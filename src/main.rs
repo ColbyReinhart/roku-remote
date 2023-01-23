34a0911ba1fe6c6eb6e3fdb88902c28f38205da3
Colby Reinhart <colbyreinhart@gmail.com>
2023-01-23 19:31:47 +0000

    Reworked handle_request to use match instead of multiway ifs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::
 	net::{IpAddr, Ipv4Addr, TcpStream, TcpListener, SocketAddr},
 	io::{Write, Read},
 	time::Duration,
-	str::Split, fs::{read_to_string, File}, path::{Path, PathBuf},
+	str::Split, fs::read_to_string
 };
 
 use local_ip_address::local_ip;
@@ -16,12 +16,12 @@ use roxmltree::Document;
 use http_request_parser::{Request, Response};
 
 static SUBNET_SEARCH_LIMIT: u8 = 15;	// Last number of subnet to check (exclusive)
+static CONTENT_TYPES: &'static [&'static str] = &["css", "js"];
 
 fn main()
 {
 	// First, search the current subnet for Roku devices
-	///let devices: Vec<RokuDevice> = find_devices();
-	let devices: Vec<RokuDevice> = Vec::new();///
+	let devices: Vec<RokuDevice> = find_devices();
 	for device in &devices
 	{
 		println!("{:?}", device);
@@ -44,167 +44,161 @@ fn handle_request(stream: &mut TcpStream, devices: & Vec<RokuDevice>)
 	let req: Request = Request::from(&stream);
 	let mut res: Response = Response::new();
 
-	println!("{:?}", req);
-
-	// If they sent a keypress request, find out what key and which device
-	// to send it to
-	if req.method == "PUT" && req.path == "/keypress"
-	{	
-		// What's the device and what key should be pressed?
-		let mut device: &str = "NULL";
-		let mut action: &str = "NULL";
-
-		// We'll assume we're getting a standard form body
-		for form_input in req.body.split("&").into_iter()
+	match (&req.method[..], &req.path[..])
+	{
+		// Send an instruction to a roku device
+		("PUT", "/keypress") =>
 		{
-			// Get the name and value of each input
-			let pair: Vec<&str> = form_input.split("=").collect();
+			// What's the device and what key should be pressed?
+			let mut device: &str = "NULL";
+			let mut action: &str = "NULL";
 
-			// Assert valid syntax
-			if pair.len() != 2
+			// We'll assume we're getting a standard form body
+			for form_input in req.body.split("&").into_iter()
+			{
+				// Get the name and value of each input
+				let pair: Vec<&str> = form_input.split("=").collect();
+
+				// Assert valid syntax
+				if pair.len() != 2
+				{
+					res.status = 400;
+					res.status_message = format!("Invalid PUT parameter: {}", pair.join("="));
+					res.send(stream);
+					return;
+				}
+
+				match pair.get(0).expect("Could not inspect PUT body parameter")
+				{
+					&"device" => device = pair.get(1).unwrap(),
+					&"action" => action = pair.get(1).unwrap(),
+					_ => () // Inclusion of additional form data is unnecessary but
+							// shouldn't break anything
+				}
+			}
+
+			// Verify that we have the data we need
+			if device == "NULL" || action == "NULL"
 			{
 				res.status = 400;
-				res.status_message = format!("Invalid PUT parameter: {}", pair.join("="));
+				res.status_message = "Incorrect parameters for keypress call!".to_owned();
 				res.send(stream);
 				return;
 			}
 
-			match pair.get(0).expect("Could not inspect PUT body parameter")
+			// Now that we have the device and action, lets get the info we need
+			// and send out the command.
+			let device_info: Vec<&RokuDevice> = devices.
+				into_iter()
+				.filter(|roku| roku.name == device)
+				.collect();
+
+			// Handle the case where the device doesn't exist
+			if device_info.is_empty()
 			{
-				&"device" => device = pair.get(1).unwrap(),
-				&"action" => action = pair.get(1).unwrap(),
-				_ => () // Inclusion of additional form data is unnecessary but
-						// shouldn't break anything
+				res.status = 400;
+				res.status_message = format!("No device found with name: {}", device);
+				res.send(stream);
+				return;
 			}
-		}
 
-		// Verify that we have the data we need
-		if device == "NULL" || action == "NULL"
-		{
-			res.status = 400;
-			res.status_message = "Incorrect parameters for keypress call!".to_owned();
+			// We'll assume that Rokus can't have the same device name. If this assumption is wrong,
+			// then whichever Roku has the lower IP address (was inserted into the vector first)
+			// will be issued the command
+			let device_to_command: &&RokuDevice = device_info.get(0).unwrap();
+			let device_socket: SocketAddr =
+				SocketAddr::new(IpAddr::V4(device_to_command.address), 8060);
+			
+			// Write the request
+			let mut command: TcpStream = TcpStream::connect(device_socket).unwrap();
+			command.write("POST /keypress/".as_bytes()).unwrap();
+			command.write(action.as_bytes()).unwrap();
+			command.write(" HTTP/1.1\r\n\r\n".as_bytes()).unwrap();
+
+			// For some reason the TV takes a few seconds to respond, and we don't
+			// want to waste time listening for a response that we don't really care
+			// about. Actually, we should care, but until I figure out multithreading
+			// this is the only way to get things to run smoothly
+			res.status = 200;
 			res.send(stream);
-			return;
-		}
+		},
 
-		// Now that we have the device and action, lets get the info we need
-		// and send out the command.
-		let device_info: Vec<&RokuDevice> = devices.
-			into_iter()
-			.filter(|roku| roku.name == device)
-			.collect();
-
-		// Handle the case where the device doesn't exist
-		if device_info.is_empty()
+		// Serve index.html
+		("GET", "/") =>
 		{
-			res.status = 400;
-			res.status_message = format!("No device found with name: {}", device);
+			res.status = 200;
+			res.headers.push("Content-Type: text/html; charset=utf-8".to_owned());
+			res.body = read_to_string("static/index.html")
+				.expect("Could not open index.html");
 			res.send(stream);
-			return;
-		}
+		},
 
-		// We'll assume that Rokus can't have the same device name. If this assumption is wrong,
-		// then whichever Roku has the lower IP address (was inserted into the vector first)
-		// will be issued the command
-		let device_to_command: &&RokuDevice = device_info.get(0).unwrap();
-		let device_socket: SocketAddr =
-			SocketAddr::new(IpAddr::V4(device_to_command.address), 8060);
-		
-		// Write the request
-		let mut command: TcpStream = TcpStream::connect(device_socket).unwrap();
-		command.write("POST /keypress/".as_bytes()).unwrap();
-		command.write(action.as_bytes()).unwrap();
-		command.write(" HTTP/1.1\r\n\r\n".as_bytes()).unwrap();
-
-		// For some reason the TV takes a few seconds to respond, and we don't
-		// want to waste time listening for a response that we don't really care
-		// about. Actually, we should care, but until I figure out multithreading
-		// this is the only way to get things to run smoothly
-		res.status = 200;
-		res.send(stream);
-	}
-	// A GET to root will just serve the HTML
-	else if req.method == "GET" && req.path == "/"
-	{
-		res.status = 200;
-
-		// Read in index, set as the response body, and send it along
-		let file: String = read_to_string("static/index.html")
-			.expect("Could not open index.html");
-		res.headers.push("Content-Type: text/html; charset=utf-8".to_owned());
-		res.body = file;
-		res.send(stream);
-	}
-	// A GET to /devices will return a list of device names
-	else if req.method == "GET" && req.path == "/devices"
-	{
-		res.status = 200;
-		let device_names: Vec<String> = devices.into_iter()
-			.map(|f| f.name.to_owned())
-			.collect();
-		res.body = device_names.join(",");
-		res.send(stream);
-	}
-	// Everything else we'll try to serve from the static directory
-	else
-	{
-		// Stop evil hackers
-		if req.path.contains("../")
+		// Obtain a list of device names found by the server
+		("GET", "/devices") =>
 		{
-			res.status = 400;
-			res.status_message = "You know what you did.".to_owned();
+			res.status = 200;
+			let device_names: Vec<String> = devices.into_iter()
+				.map(|f| f.name.to_owned())
+				.collect();
+			res.body = device_names.join(",");
 			res.send(stream);
-		}
+		},
 
-		// Build the path, starting from the static folder
-		let mut path: String = String::from("static");
-		path.push_str(&req.path);
-
-		// Try to get the file the user has requested
-		match File::open(path)
+		// A GET to an arbitrary path will try to serve from the static directory
+		("GET", _) =>
 		{
-			Ok(mut requested_file) => 
+			// Stop evil hackers
+			if req.path.contains("../")
 			{
-				// Read the file into a string and set it as the body
-				let mut file_contents: String = String::new();
-				match requested_file.read_to_string(&mut file_contents)
-				{
-					Ok(_) => {},
-					Err(_) =>
-					{
-						res.status = 500;
-						res.status_message = "Internal Server Error".to_owned();
-						res.send(stream);
-						return;
-					},
-				}
+				res.status = 400;
+				res.status_message = "You know what you did.".to_owned();
+				res.send(stream);
+			}
 
-				// Set content headers if needed
-				match &req.path.split(".").into_iter().last()
+			// Build the path, starting from the static folder
+			let mut path: String = String::from("static");
+			path.push_str(&req.path);
+
+			// Read in the contents of the file
+			match read_to_string(path)
+			{
+				Ok(file_contents) =>
 				{
-					Some(extension) =>
+					// Set content headers if needed
+					match &req.path.split(".").into_iter().last()
 					{
-						match extension
+						Some(extension) =>
 						{
-							&"js" => res.headers.push("Content-Type: text/js".to_owned()),
-							&"css" => res.headers.push("Content-Type: text/css".to_owned()),
-							_ => {}
-						}
-					},
-					None => {}
-				}
+							if CONTENT_TYPES.contains(extension)
+							{
+								let mut content_type: String = String::from("Content-Type: text/");
+								content_type.push_str(extension);
+								res.headers.push(content_type);
+							}
+						},
+						None => {}
+					}
 
-				// Send the response
-				res.body = file_contents;
-				res.send(stream);
-			},
-			Err(_) =>
-			{
-				res.status = 404;
-				res.status_message = "File not found".to_owned();
-				res.send(stream);
-			},
-		};
+					res.body = file_contents;
+					res.send(stream);
+				},
+				Err(_) =>
+				{
+					res.status = 404;
+					res.status_message = "Not Found".to_owned();
+					res.send(stream);
+					return;
+				}
+			}
+		},
+
+		// Anything else is definitely a 404
+		(_, _) =>
+		{
+			res.status = 404;
+			res.status_message = "Not Found".to_owned();
+			res.send(stream);
+		}
 	}
 }
 

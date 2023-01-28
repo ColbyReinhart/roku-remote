2033142d13b8a377e40480019085948163624c07
Colby Reinhart <colbyreinhart@gmail.com>
2023-01-28 13:13:11 +0000

    Working on text input functionality

--- static/index.js
+++ static/index.js
@@ -4,19 +4,49 @@ let xmlHttp = new XMLHttpRequest();
 xmlHttp.open("GET", "/devices", false);
 xmlHttp.send(null);
 xmlHttp.responseText.split("\r\n")[0].split(",").forEach((device) => {
-    let newOption = document.createElement("option");
-    newOption.setAttribute("value", device);
-    newOption.appendChild(document.createTextNode(device));
-    dropdown.appendChild(newOption);
+	let newOption = document.createElement("option");
+	newOption.setAttribute("value", device);
+	newOption.appendChild(document.createTextNode(device));
+	dropdown.appendChild(newOption);
 });
 
 // Register an event listener on every keypress button
 // to make it control the TV
 document.querySelectorAll(".keypress").forEach((button) => {
-    const command = button.id;
-    button.addEventListener("click", () => {
-        let commandReq = new XMLHttpRequest();
-        commandReq.open("PUT", "/keypress", false);
-        commandReq.send("device=" + document.querySelector("#deviceList").value + "&action=" + command);
-    });
+	const command = button.id;
+	button.addEventListener("click", () => {
+		let commandReq = new XMLHttpRequest();
+		commandReq.open("PUT", "/keypress", false);
+		commandReq.send("device=" + document.querySelector("#deviceList").value + "&action=" + command);
+	});
+});
+
+// Menu control
+document.querySelector("#Features").addEventListener("click", () => {
+	let menu = document.querySelector("#menu");
+	if (menu.style.display == "none" || menu.style.display == "")
+	{
+		menu.style.display = "flex";
+	}
+	else
+	{
+		menu.style.display = "none";
+	}
+});
+
+document.querySelector("#text").addEventListener("input", (event) => {
+	let commandReq = new XMLHttpRequest();
+	const device = document.querySelector("#deviceList").value;
+
+	// If the user typed a letter, number, or other symbol
+	// if (event.key.length == 1)
+	// {
+	// 	commandReq.open("PUT", "/keypress", false);
+	// 	commandReq.send("device=" + device + "&action=Lit_" + event.key);
+	// }
+	// else if (event.key == "Backspace" || event.key == "Enter")
+	// {
+	// 	commandReq.open("PUT", "/keypress", false);
+	// 	commandReq.send("device=" + device + "&action=" + event.key);
+	// }
 });
